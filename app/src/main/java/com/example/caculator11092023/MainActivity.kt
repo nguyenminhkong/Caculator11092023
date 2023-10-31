06b265e3cc2a9234239c6d7f1ddd95ada41fa1df
Kogi Nguyen <nguyenminhkong1994@gmail.com>
2023-10-31 21:35:28 +0700

    caculator main

--- app/src/main/java/com/example/caculator11092023/MainActivity.kt
+++ app/src/main/java/com/example/caculator11092023/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.caculator11092023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    //Global scope
+    private lateinit var edtNumber1: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Local scope
+        //Lay gia tri ra tu resource
+        //Ánh xạ
+        edtNumber1 = findViewById(R.id.edit_text_number1)
+        edtNumber1.setText("Text")
     }
 }
